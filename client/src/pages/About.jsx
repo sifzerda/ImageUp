c16faf72a4ffa1c1ef8e59eaf8602250729f5ee3
Troy <tydamon@hotmail.com>
2024-07-31 04:44:47 +1000

    //

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,6 +7,10 @@ export default function About () {
   return (
     <div>
 
+      <p>Consider using this page to grab/display recently uploaded pictures (by all users)</p>
+      <p>However, this would mean creating a new model called Image which is connected to user models but holds all image urls</p>
+<p>probably better to implement that after main app working and images going up to cloud</p>
+
 {/* game component */}
 
 <div className='grid-wrapper'>
